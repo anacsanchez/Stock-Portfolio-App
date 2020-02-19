e552b59e170cdf8a80e353db38b3a1ec8b2ae2b9
Ana C Sanchez <ana@anacsanchez.com>
2020-02-19 15:17:48 -0500

    added app component to test apollo hooks

--- client/app.js
+++ client/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const GET_STOCKS = gql`
+query getStocks($symbols:[String!]) {
+  getStocks(symbols: $symbols) {
+      stocks {
+      symbol
+      price
+      }
+      success
+      message
+  }
+}`;
 
 export default function App () {
+  const { data, loading, error} = useQuery(GET_STOCKS, { variables: {"symbols": ["aalz","twtr"]} });
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>ERROR</p>
+  if(!data) return <p>Not found</p>
+
   return (
-    <div>Hi</div>
+    <div>Hi
+      {JSON.stringify(data)}
+    </div>
   );
 }
+
--- client/index.js
+++ client/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: "http://localhost:4000/graphql"
+});
+
+const client = new ApolloClient({
+    cache,
+    link
+});
+
+// client.query({
+//     query: gql`
+//     query GetStocks($symbols:[String!]) {
+//     getStocks(symbols: $symbols) {
+//         stocks {
+//         symbol
+//         price
+//         }
+//         success
+//         message
+//     }
+//     }
+//     `,
+//     variables: {
+//         "symbols": ["aalz","twtr"]
+//     }
+// })
+// .then(result => console.log(result))
+// .catch(err => console.log(err))
 
 ReactDOM.render(
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 ,document.getElementById('app'));
