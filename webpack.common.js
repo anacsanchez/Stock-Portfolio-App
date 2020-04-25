e45d06d26d8ca06e8cc6919b76784502984f4978
Ana C Sanchez <ana@anacsanchez.com>
2020-04-25 13:13:14 -0400

    moved public out of client folder and modified webpack configs for new folder structure

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: path.join(__dirname, 'src','client','index.js'),
   output: {
-    path: path.resolve(__dirname, 'client','public'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'client', 'public'),
+    contentBase: path.resolve(__dirname, 'public'),
     hot: true,
     port: 8080,
     proxy: {
